b6eb1ec60373272935e4d27a0daad60d33bb8d2e
Pierre Lang <couseragit@pierre-lang.info>
2025-05-21 08:47:21 -0400

    new claculator change

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by coursera-guit-2025
+# Here is a new update by coursera-git-2025
+
+# Here is another update by coursera-git-2025
