542bd371b50e206f83ca78d85456368621bdd012
Pierre Lang <couseragit@pierre-lang.info>
2025-05-21 08:47:50 -0400

    Revert "new claculator change"
    This reverts commit b6eb1ec60373272935e4d27a0daad60d33bb8d2e.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by coursera-git-2025
-
-# Here is another update by coursera-git-2025
+# Here is a new update by coursera-guit-2025
